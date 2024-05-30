f7b6e8fde1c6893216bc73e7fc319e953a634c27
Achilleas Koutsou <achilleas@koutsou.net>
2024-05-30 19:49:05 +0200

    otk-gen-partition-table: remove base partition table
    The base partition table is read as input.

--- cmd/otk-gen-partition-table/main.go
+++ cmd/otk-gen-partition-table/main.go
@@ -14,60 +14,6 @@ import (
 	"github.com/osbuild/images/pkg/osbuild"
 )
 
-// TODO: read  this from the input - the tool itself should not have a base PT
-var basePt = disk.PartitionTable{
-	UUID: "D209C89E-EA5E-4FBD-B161-B461CCE297E0",
-	Type: "gpt",
-	Partitions: []disk.Partition{
-		{
-			Size:     1 * common.MebiByte,
-			Bootable: true,
-			Type:     disk.BIOSBootPartitionGUID,
-			UUID:     disk.BIOSBootPartitionUUID,
-		},
-		{
-			Size: 200 * common.MebiByte,
-			Type: disk.EFISystemPartitionGUID,
-			UUID: disk.EFISystemPartitionUUID,
-			Payload: &disk.Filesystem{
-				Type:         "vfat",
-				UUID:         disk.EFIFilesystemUUID,
-				Mountpoint:   "/boot/efi",
-				Label:        "EFI-SYSTEM",
-				FSTabOptions: "defaults,uid=0,gid=0,umask=077,shortname=winnt",
-				FSTabFreq:    0,
-				FSTabPassNo:  2,
-			},
-		},
-		{
-			Size: 500 * common.MebiByte,
-			Type: disk.FilesystemDataGUID,
-			UUID: disk.FilesystemDataUUID,
-			Payload: &disk.Filesystem{
-				Type:         "ext4",
-				Mountpoint:   "/boot",
-				Label:        "boot",
-				FSTabOptions: "defaults",
-				FSTabFreq:    0,
-				FSTabPassNo:  0,
-			},
-		},
-		{
-			Size: 2 * common.GibiByte,
-			Type: disk.FilesystemDataGUID,
-			UUID: disk.RootPartitionUUID,
-			Payload: &disk.Filesystem{
-				Type:         "ext4",
-				Label:        "root",
-				Mountpoint:   "/",
-				FSTabOptions: "defaults",
-				FSTabFreq:    0,
-				FSTabPassNo:  0,
-			},
-		},
-	},
-}
-
 type OtkGenPartitionInput struct {
 	Options    *OtkPartOptions `json:"options"`
 	Partitions []*OtkPartition `json:"partitions"`
