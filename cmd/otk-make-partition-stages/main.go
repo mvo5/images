53dd0aa1025ab2006a30ada6f3f22d686d07e0db
Michael Vogt <michael.vogt@gmail.com>
2024-06-04 16:02:07 +0200

    otk-make-partition-stages: add new `otk-make-partition-stages` external
    This external will consume the data from `otk-gen-partition-table`
    and generate `osbuild` stages from it.
    
    The expected usage is something like:
    ```yaml
    otk.define:
     modifications:
      filesystem:
        min_disk_size: 20 GiB
    
    otk.define.defaults:
     filesystem:
       otk.external.gen_partition_table:
         modifications: ${modifications.filesystem}
         properties: ...
         partitions: ...
    
    stages:
     ...
     otk.external.make_partition_stages:
       internal: ${filesystem.internal}
       # TODO: add filename here
    ```

---
+++ cmd/otk-make-partition-stages/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/osbuild/images/pkg/disk"
+	"github.com/osbuild/images/pkg/osbuild"
+)
+
+// keep in sync with "otk-gen-partition-table"
+type Input struct {
+	Internal InputInternal `json:"internal"`
+}
+
+type InputInternal struct {
+	PartitionTable *disk.PartitionTable `json:"partition-table"`
+}
+
+func makeImagePrepareStages(inp Input, filename string) (stages []*osbuild.Stage, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("cannot generate image prepare stages: %v", r)
+		}
+	}()
+
+	// XXX: would we ever want to change that?
+	partTool := osbuild.PTSfdisk
+	stages = osbuild.GenImagePrepareStages(inp.Internal.PartitionTable, filename, partTool)
+	return stages, nil
+}
+
+func run(r io.Reader, w io.Writer) error {
+	var inp Input
+	if err := json.NewDecoder(r).Decode(&inp); err != nil {
+		return err
+	}
+
+	// XXX: make configurable via "modification"
+	fname := "disk.img"
+	stages, err := makeImagePrepareStages(inp, fname)
+	if err != nil {
+		return fmt.Errorf("cannot make partition stages: %w", err)
+	}
+
+	outputJson, err := json.MarshalIndent(stages, "", "  ")
+	if err != nil {
+		return fmt.Errorf("cannot marshal response: %w", err)
+	}
+	fmt.Fprintf(w, "%s\n", outputJson)
+	return nil
+}
+
+func main() {
+	if err := run(os.Stdin, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err.Error())
+		os.Exit(1)
+	}
+}
