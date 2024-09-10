216bab68f0fe76778fa212903a18278a0fd9f75e
Achilleas Koutsou <achilleas@koutsou.net>
2024-09-10 13:42:24 +0200

    disk: add PartitionTableType to CustomPartitionTableOptions
    Add an option for the partition table type to the
    CustomPartitionTableOptions.  The value must be one of "gpt", "dos", or
    "".  An empty string (or nil options) defaults to "gpt".
    
    The tests have been updated to explicitly specify "dos".  The relayout()
    method, which is affected by this, checks whether `pt.Type == "gpt"`,
    which means it was defaulting to "dos" when deciding to add a footer.

--- pkg/disk/partition_table.go
+++ pkg/disk/partition_table.go
@@ -188,6 +188,9 @@ func mkESP(size uint64) Partition {
 }
 
 type CustomPartitionTableOptions struct {
+	// PartitionTableType must be either "dos" or "gpt". Defaults to "gpt".
+	PartitionTableType string
+
 	// BootMode determines the types of boot-related partitions that are
 	// automatically added, BIOS boot (legacy), ESP (UEFI), or both (hybrid).
 	// If none, no boot-related partitions are created.
@@ -207,11 +210,23 @@ type CustomPartitionTableOptions struct {
 func NewCustomPartitionTable(customizations *blueprint.PartitioningCustomization, options *CustomPartitionTableOptions, rng *rand.Rand) (*PartitionTable, error) {
 	if options == nil {
 		// init options with empty struct to use defaults
-		options = &CustomPartitionTableOptions{}
+		options = &CustomPartitionTableOptions{
+			PartitionTableType: "gpt",
+		}
 	}
-	// TODO: handle dos pt type
 
 	pt := &PartitionTable{}
+
+	switch options.PartitionTableType {
+	case "gpt", "dos":
+		pt.Type = options.PartitionTableType
+	case "":
+		// default to "gpt"
+		pt.Type = "gpt"
+	default:
+		return nil, fmt.Errorf("invalid partition table type specified when generating partition table: %s", options.PartitionTableType)
+	}
+
 	switch options.BootMode {
 	case platform.BOOT_LEGACY:
 		// add BIOS boot partition
