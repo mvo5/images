de6573b8e7c817d007ac06245e44a278ac2fc123
Michael Vogt <michael.vogt@gmail.com>
2024-10-30 16:28:19 +0100

    filters_fmt: add new NewFilteredResultFormatter()
    This commit adds a FilteredResultFormatter interface that allows
    to add new formatter to filter results. Initially a "text" and
    a "json" formatter are supported.

---
+++ cmd/image-builder/filters_fmt.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+)
+
+type FilteredResultFormatter interface {
+	Output(io.Writer, []FilterResult) error
+}
+
+func NewFilteredResultFormatter(format string) (FilteredResultFormatter, error) {
+	switch format {
+	case "", "text":
+		return &textFilteredResultFormatter{}, nil
+	case "json":
+		return &jsonFilteredResultFormatter{}, nil
+	default:
+		return nil, fmt.Errorf("unsupported formatter %q", format)
+	}
+}
+
+type textFilteredResultFormatter struct{}
+
+func (*textFilteredResultFormatter) Output(w io.Writer, all []FilterResult) error {
+	var errs []error
+	for _, res := range all {
+		if _, err := fmt.Fprintf(w, "%s --arch %s --type %s\n", res.Distro.Name(), res.Arch.Name(), res.ImgType.Name()); err != nil {
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+
+	return nil
+}
+
+type jsonFilteredResultFormatter struct{}
+
+// XXX: consider adding more details to the output
+type filteredResultJSON struct {
+	Distro struct {
+		Name string `json:"name"`
+	} `json:"distro"`
+	Arch struct {
+		Name string `json:"name"`
+	} `json:"arch"`
+	ImgType struct {
+		Name string `json:"name"`
+	} `json:"image_type"`
+}
+
+func (*jsonFilteredResultFormatter) Output(w io.Writer, all []FilterResult) error {
+	var out []filteredResultJSON
+
+	for _, res := range all {
+		out = append(out, filteredResultJSON{
+			Distro: struct {
+				Name string `json:"name"`
+			}{
+				Name: res.Distro.Name(),
+			},
+			Arch: struct {
+				Name string `json:"name"`
+			}{
+				Name: res.Arch.Name(),
+			},
+			ImgType: struct {
+				Name string `json:"name"`
+			}{
+				Name: res.ImgType.Name(),
+			},
+		})
+	}
+
+	enc := json.NewEncoder(w)
+	return enc.Encode(out)
+}
---
+++ cmd/image-builder/filters_fmt_test.go
@@ -0,0 +1,44 @@
+package main_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/osbuild/images/cmd/image-builder"
+	"github.com/osbuild/images/pkg/distrofactory"
+)
+
+func TestFitleredResultFormatter(t *testing.T) {
+	fac := distrofactory.NewTestDefault()
+
+	for _, tc := range []struct {
+		formatter             string
+		distro, arch, imgType string
+		expectsOutput         string
+	}{
+		{"", "test-distro-1", "test_arch3", "qcow2", `test-distro-1 --arch test_arch3 --type qcow2` + "\n"},
+		{"text", "test-distro-1", "test_arch3", "qcow2", `test-distro-1 --arch test_arch3 --type qcow2` + "\n"},
+		{"json", "test-distro-1", "test_arch3", "qcow2", `[{"distro":{"name":"test-distro-1"},"arch":{"name":"test_arch3"},"image_type":{"name":"qcow2"}}]` + "\n"},
+	} {
+		// XXX: it would be nice if TestDistro would support constructing
+		// like GetDistro("rhel-8.1:i386,amd64:ami,qcow2") instead of
+		// the current very static setup
+		di := fac.GetDistro(tc.distro)
+		require.NotNil(t, di)
+		ar, err := di.GetArch(tc.arch)
+		require.NoError(t, err)
+		im, err := ar.GetImageType(tc.imgType)
+		require.NoError(t, err)
+
+		var buf bytes.Buffer
+		res := []main.FilterResult{{di, ar, im}}
+		fmter, err := main.NewFilteredResultFormatter(tc.formatter)
+		require.NoError(t, err)
+		err = fmter.Output(&buf, res)
+		assert.NoError(t, err)
+		assert.Equal(t, tc.expectsOutput, buf.String(), tc)
+	}
+}
