66c61347c2aed908f492175dde8f167d41c966ea
Michael Vogt <michael.vogt@gmail.com>
2025-01-21 09:55:47 +0100

    manifestgen: add support for CustomSeed via options
    This commit adds support for custom seed arguments via the
    `manifesten.Options` to support use-cases like `cmd/gen-manifests`.

--- pkg/manifestgen/manifestgen.go
+++ pkg/manifestgen/manifestgen.go
@@ -115,6 +115,10 @@ type Options struct {
 	// filename contains the suggest filename string and the
 	// content can be read
 	SBOMWriter SBOMWriterFunc
+
+	// CustomSeed overrides the default rng seed, this is mostly
+	// useful for testing
+	CustomSeed *int64
 }
 
 // Generator can generate an osbuild manifest from a given repository
@@ -131,6 +135,8 @@ type Generator struct {
 	reporegistry *reporegistry.RepoRegistry
 
 	rpmDownloader osbuild.RpmDownloader
+
+	customSeed *int64
 }
 
 // New will create a new manifest generator
@@ -148,6 +154,7 @@ func New(reporegistry *reporegistry.RepoRegistry, opts *Options) (*Generator, er
 		commitResolver:    opts.CommitResolver,
 		rpmDownloader:     opts.RpmDownloader,
 		sbomWriter:        opts.SBOMWriter,
+		customSeed:        opts.CustomSeed,
 	}
 	if mg.out == nil {
 		mg.out = os.Stdout
@@ -176,7 +183,7 @@ func (mg *Generator) Generate(bp *blueprint.Blueprint, dist distro.Distro, imgTy
 	if err != nil {
 		return err
 	}
-	preManifest, warnings, err := imgType.Manifest(bp, *imgOpts, repos, nil)
+	preManifest, warnings, err := imgType.Manifest(bp, *imgOpts, repos, mg.customSeed)
 	if err != nil {
 		return err
 	}
