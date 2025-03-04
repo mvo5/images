22a9964dfcaf95182e71e50f8f39109f5643eb73
Michael Vogt <michael.vogt@gmail.com>
2025-03-04 10:59:29 +0100

    common: add common.MajorVersionEqual()

--- internal/common/distro.go
+++ internal/common/distro.go
@@ -30,3 +30,16 @@ func VersionLessThan(a, b string) bool {
 func VersionGreaterThanOrEqual(a, b string) bool {
 	return !VersionLessThan(a, b)
 }
+
+func MajorVersionEqual(a, b string) bool {
+	aV, err := version.NewVersion(a)
+	if err != nil {
+		panic(err)
+	}
+	bV, err := version.NewVersion(b)
+	if err != nil {
+		panic(err)
+	}
+
+	return aV.Segments()[0] == bV.Segments()[0]
+}
