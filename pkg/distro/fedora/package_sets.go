07a179ce1308acc5e97d93fbf804d11d34138cb2
Michael Vogt <michael.vogt@gmail.com>
2025-05-08 12:18:48 +0200

    fedora: add support to load ImageTypes fully from YAML
    This commits adds the ability to load image types fully from
    YAML and moves the `container` type into pure YAML as the
    first example.

--- pkg/distro/fedora/package_sets.go
+++ pkg/distro/fedora/package_sets.go
@@ -1,6 +1,8 @@
 package fedora
 
 import (
+	"fmt"
+
 	"github.com/osbuild/images/internal/common"
 	"github.com/osbuild/images/pkg/distro"
 	"github.com/osbuild/images/pkg/distro/defs"
@@ -15,4 +17,37 @@ func imageConfig(d distribution, imageType string) *distro.ImageConfig {
 	// arch is currently not used in fedora
 	arch := ""
 	return common.Must(defs.ImageConfig(d.name, arch, imageType, VersionReplacements()))
+}
+
+func newImageTypeFrom(d distribution, imgYAML defs.ImageTypeYAML) imageType {
+	it := imageType{
+		name:                   imgYAML.Name(),
+		filename:               imgYAML.Filename,
+		mimeType:               imgYAML.MimeType,
+		bootable:               imgYAML.Bootable,
+		defaultSize:            imgYAML.DefaultSize,
+		buildPipelines:         imgYAML.BuildPipelines,
+		payloadPipelines:       imgYAML.PayloadPipelines,
+		exports:                imgYAML.Exports,
+		requiredPartitionSizes: imgYAML.RequiredPartitionSizes,
+		environment:            &imgYAML.Environment,
+	}
+	// XXX: make this a helper on imgYAML()
+	it.defaultImageConfig = imageConfig(d, imgYAML.Name())
+	// XXX: fix for multiple loaders like the installers
+	it.packageSets = map[string]packageSetFunc{
+		osPkgsKey: packageSetLoader,
+	}
+
+	switch imgYAML.Image {
+	case "disk":
+		it.image = diskImage
+	case "container":
+		it.image = containerImage
+	default:
+		err := fmt.Errorf("unknown image func: %v for %v", imgYAML.Image, imgYAML.Name())
+		panic(err)
+	}
+
+	return it
 }
