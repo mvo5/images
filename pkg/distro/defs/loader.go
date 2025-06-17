5e031eb34d4c78068a4d4381cd9a6553b094ac6e
Michael Vogt <michael.vogt@gmail.com>
2025-06-17 12:12:34 +0200

    distro: drop no longer used functions
    This commit drops the "backward" and "versionLessThanSortedKeys"
    functions that are no longer needed.

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -302,37 +302,6 @@ type partitionTablesOverwriteCondition struct {
 	Override map[string]*disk.PartitionTable `yaml:"override"`
 }
 
-// XXX: use slices.Backward() once we move to go1.23
-// hint: use "git blame" on this comment and just revert
-// the commit that adds it and you will have the 1.23 version
-func backward[Slice ~[]E, E any](s Slice) []E {
-	out := make([]E, 0, len(s))
-	for i := len(s) - 1; i >= 0; i-- {
-		out = append(out, s[i])
-	}
-	return out
-}
-
-// XXX: use slices.SortedFunc() once we move to go1.23
-// hint: use "git blame" on this comment and just revert
-// the commit that adds it and you will have the 1.23 version
-func versionLessThanSortedKeys[T any](m map[string]T) []string {
-	versions := maps.Keys(m)
-	slices.SortFunc(versions, func(a, b string) int {
-		ver1 := version.Must(version.NewVersion(a))
-		ver2 := version.Must(version.NewVersion(b))
-		switch {
-		case ver1 == ver2:
-			return 0
-		case ver2.LessThan(ver1):
-			return -1
-		default:
-			return 1
-		}
-	})
-	return versions
-}
-
 type evalEnv struct {
 	DistroID *distro.ID
 	Arch     string
