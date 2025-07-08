c704638e658cba95252cca6640d5a502ff84c60a
Michael Vogt <michael.vogt@gmail.com>
2025-07-08 12:38:18 +0200

    distro: export defs.DistrosYAML

--- pkg/distro/defs/loader.go
+++ pkg/distro/defs/loader.go
@@ -50,8 +50,8 @@ func dataFS() fs.FS {
 	return dataFS
 }
 
-// distrosYAML defines all supported YAML based distributions
-type distrosYAML struct {
+// DistrosYAML defines all supported YAML based distributions
+type DistrosYAML struct {
 	Distros []DistroYAML
 }
 
@@ -159,7 +159,7 @@ func NewDistroYAML(nameVer string) (*DistroYAML, error) {
 	decoder := yaml.NewDecoder(f)
 	decoder.KnownFields(true)
 
-	var distros distrosYAML
+	var distros DistrosYAML
 	if err := decoder.Decode(&distros); err != nil {
 		return nil, err
 	}
