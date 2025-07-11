c017995a9d01ad62dde87fc0ebf4ff8ba1dfb0e3
Michael Vogt <michael.vogt@gmail.com>
2025-07-11 10:30:53 +0200

    bootc: use container introspection to get name/ver

--- pkg/distro/bootc/bootc.go
+++ pkg/distro/bootc/bootc.go
@@ -1,13 +1,27 @@
 package bootc
 
 import (
+	"fmt"
+
+	"github.com/osbuild/images/pkg/bib/container"
+	"github.com/osbuild/images/pkg/bib/osinfo"
 	"github.com/osbuild/images/pkg/distro/generic"
 	"github.com/osbuild/images/pkg/imagefilter"
 )
 
 func FromRef(bootcRef, imgTypeStr, archStr string) (*imagefilter.Result, error) {
-	// XXX: get by actually introspecting
-	nameVer := "bootc-1"
+	cnt, err := container.New(bootcRef)
+	if err != nil {
+		return nil, err
+	}
+	defer cnt.Stop()
+
+	info, err := osinfo.Load(cnt.Root())
+	if err != nil {
+		return nil, err
+	}
+
+	nameVer := fmt.Sprintf("bootc-%s-%s", info.OSRelease.ID, info.OSRelease.VersionID)
 	distro, err := generic.NewDistroForBootc(nameVer, "./bootc")
 	if err != nil {
 		return nil, err
